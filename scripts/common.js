dded51097e054ea8757d75833bc84b7de500e091
William Forsyth <info@willforsyth.co.uk>
2016-01-31 16:10:50 +0000

    Updates

--- scripts/common.js
+++ scripts/common.js
@@ -372,27 +372,52 @@ $.fn.revealText = function (options) {
 
     $('.fourth-link').on('click', function(e){
       e.preventDefault();
-      $('.swing-fifth').toggleClass('flip');
-      $('.front-fifth').toggleClass('clicked');
-      $('.swing-fifth .back').fadeOut(1000);
+      $('.swing-fourth-odd').toggleClass('flip');
+      // $('.front-fourth-odd').toggleClass('clicked');
+      $('.swing-fourth-odd .back').fadeOut(1000);
+      $('.swing-fifth').delay(2000).queue(function(){
+            $(this).addClass('flip');
+            $('.front-fifth').addClass('clicked');
+            $('.swing-fifth .back').fadeOut(1000) .clearQueue();
+        });
     });
 
     $('.fifth-link').on('click', function(e){
       e.preventDefault();
       $('.swing-sixth').addClass('flip');
-      $('.front-fifth').addClass('clicked');
+      $('.front-sixth').addClass('clicked');
       $('.swing-sixth .back').fadeOut(1000);
-        $('.swing-seventh').delay(2000).queue(function(){
+        $('.swing-sixth-odd').delay(2000).queue(function(){
+            $(this).addClass('flip');
+            $('.front-sixth-odd').addClass('clicked');
+            $('.swing-sixth-odd .back').fadeOut(1000) .clearQueue();
+        });
+          $('.swing-seventh').delay(4000).queue(function(){
             $(this).addClass('flip');
             $('.front-seventh').addClass('clicked');
             $('.swing-seventh .back').fadeOut(1000) .clearQueue();
         });
-          $('.swing-eight').delay(4000).queue(function(){
-            $(this).addClass('flip');
-            $('.front-eight').addClass('clicked');
-            $('.swing-eigth .back').fadeOut(1000) .clearQueue();
-        });
+        $('.swing-eight').delay(6000).queue(function(){
+          $(this).addClass('flip');
+          $('.front-eight').addClass('clicked');
+          $('.swing-eigth .back').fadeOut(1000) .clearQueue();
+      });
     });
 };
+
+
+// $('.swing-sixth').addClass('flip');
+// $('.front-fifth').addClass('clicked');
+// $('.swing-sixth .back').fadeOut(1000);
+//   $('.swing-seventh').delay(2000).queue(function(){
+//       $(this).addClass('flip');
+//       $('.front-seventh').addClass('clicked');
+//       $('.swing-seventh .back').fadeOut(1000) .clearQueue();
+//   });
+//     $('.swing-eight').delay(4000).queue(function(){
+//       $(this).addClass('flip');
+//       $('.front-eight').addClass('clicked');
+//       $('.swing-eigth .back').fadeOut(1000) .clearQueue();
+//   });
 
 },{}]},{},[4]);
